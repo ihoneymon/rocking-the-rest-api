459a3732a31c04f28d76862a00fa25fa1396c4e4
ihoneymon <ihoneymon@gmail.com>
2014-02-22 11:24:11 +0900

    PostTest 케이스 수정

--- src/test/java/kr/pe/ihoney/jco/restapi/domain/PostTest.java
+++ src/test/java/kr/pe/ihoney/jco/restapi/domain/PostTest.java
@@ -9,34 +9,34 @@ import org.junit.Test;
 public class PostTest {
 
     private User user;
-    private Group community;
+    private Group group;
     private Member member;
-    
+
     @Before
     public void setUp() {
         user = new User("tester", "test@test.email");
-        community = new Group("Test Team", GroupType.PUBLIC, user);
-        member = new Member("Tester", community, user);
+        group = new Group("Test Team", GroupType.PUBLIC, user);
+        member = new Member("Tester", group, user);
     }
 
-    @Test(expected=IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void 제목이_null인_경우() {
         @SuppressWarnings("unused")
-        Post post = new Post(null, "article", member);
+        Post post = new Post(group, null, "article", member);
         fail();
     }
-    
-    @Test(expected=IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void 본문이_null인_경우() {
         @SuppressWarnings("unused")
-        Post post = new Post("title", null, member);
+        Post post = new Post(group, "title", null, member);
         fail();
     }
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void 생성자가_null인_경우() {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void 작성자가_null인_경우() {
         @SuppressWarnings("unused")
-        Post post = new Post("title", "article", null);
+        Post post = new Post(group, "title", "article", null);
         fail();
     }
 
